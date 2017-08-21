6b7664f9a0692db236cbf6a030a0df4636f54794
Rana Saha <rsaha.nitdgp@gmail.com>
2017-08-21 09:18:00 +0800

    Code looks better now

--- DQUERY.cpp
+++ DQUERY.cpp
@@ -1,35 +1,27 @@
-#ifdef ONLINE_JUDGE
-#include <bits/stdc++.h>
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <bitset>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iomanip>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-#else
-#include "header.h"
-#endif
-#define SS(n) scanf("%s",n)
-#define pn putchar('\n')
-#define ps putchar(' ')
-#define sz(s) int((s).size())
-#define rep(i,a,b) for(int(i)=a;(i)<=(b);++(i))
-#define per(i,b,a) for(int(i)=b;(i)>=(a);--(i))
-#define clr(a) memset(a,0,sizeof(a))
-#define all(x) (x).begin(),(x).end()
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-#ifndef DEBUG
-#ifndef ONLINE_JUDGE
-#include "debug.h"
-#endif
-#else
-#define ck1
-#define ck2
-#define ck3
-#define dir
-#define show(x)
-#define show2(x,y)
-#define show3(x,y,z)
-#define show4(x,y,z,w)
-#endif
+
 
 inline int inp() {int n=0,s=1;char p=getchar_unlocked();if(p=='-')s=-1;while((p<'0'||p>'9')&&p!=EOF&&p!='-')p=getchar_unlocked();if(p=='-')s=-1,p=getchar_unlocked();while(p>='0'&&p<='9')n=(n<<3)+(n<<1)+(p-'0'),p=getchar_unlocked();return n*s;}
 inline void out(int x){if(x<0)putchar_unlocked('-'),x=-x;int ll=0,dd[10];while(x)dd[ll++]=x%10,x/=10;if(!ll)dd[ll++]=0;while(ll--)putchar_unlocked(dd[ll]+48);}
@@ -118,6 +110,7 @@ int main() {
     ans[qq[i].idx] = query(1,n,qq[i].beg,qq[i].end,1);
   }
   for(int i=1; i<=Quora; ++i) {
-    out(ans[i]); pn;
+    out(ans[i]);
+    puts("");
   }
 }
